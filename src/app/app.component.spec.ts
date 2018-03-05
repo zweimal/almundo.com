b5652d8ffeb4564db26df894c1d5b9dccfd0b4d4
German Allemand <zweimal@gmail.com>
2018-03-04 22:02:35 -0300

    Added Navbar and common styles

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,15 +17,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'AlMundo.com'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('AlMundo.com');
-  }));
-  it('should render title in a h1 tag', async(() => {
+  it('should render navbar with logo', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('AlMundo.com');
+    expect(compiled.querySelector('.navbar .logo')).toBeTruthy();
   }));
 });
